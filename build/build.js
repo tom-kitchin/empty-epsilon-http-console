b37723e24847c0776449a1c602500edbc986ae09
Tom Kitchin <tkitchin@gmail.com>
2017-06-15 20:48:33 +0100

    Get the build process to build files for local use

--- build/build.js
+++ build/build.js
@@ -28,8 +28,8 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
 
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
+      '  Tip: This has been built for local handling over file://.\n' +
+      '  Serving files via webserver may not work correctly.\n'
     ))
   })
 })
--- src/components/StateDisplay.vue
+++ src/components/StateDisplay.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Hello
+    Hello everyone
     {{ state }}
   </div>
 </template>
