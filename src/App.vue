6cf94d9c6ad14c433ba12212c4e4597585dce873
Tom Kitchin <tom.kitchin@create.net>
2017-06-15 14:06:04 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <h1>GM Console</h1>
+    <state-display :state="state" />
+  </div>
+</template>
+
+<script>
+import StateDisplay from './components/StateDisplay.vue'
+
+export default {
+  name: 'app',
+  props: {
+    config: {
+      required: false,
+      type: Object,
+      default: {}
+    }
+  },
+  data () {
+    return {
+      state: {
+        hullStrength: 50,
+        torpedoes: 5,
+        engineeringOccupied: true,
+        gameState: 'mainBattle'
+      }
+    }
+  },
+  components: {
+    StateDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/StateDisplay.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Hello
+    {{ state }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'state-display',
+  props: {
+    state: {
+      required: true,
+      type: Object
+    }
+  }
+}
+</script>
